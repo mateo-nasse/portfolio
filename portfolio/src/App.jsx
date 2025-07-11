fb6d69a189ea3e1ce449bf277a2dd3dc9f2dfde7
Mateo <M.A.Nasse@student.tudelft.nl>
2025-07-12 00:49:14 +0200

    Added experience and projects sections with dummy data

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,6 +3,8 @@ import styles from './App.module.css'
 import { Navbar } from './components/Navbar/Navbar'
 import { Hero } from './components/Hero/Hero'
 import { About } from './components/About/About'
+import { Experience } from './components/Experience/Experience'
+import { Projects } from './components/Projects/Projects'
 
 function App() {
 
@@ -11,6 +13,8 @@ function App() {
       <Navbar />
       <Hero />
       <About />
+      <Experience />
+      <Projects />
     </div>
   )
 }
